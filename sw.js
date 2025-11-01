3822fe95ed3d8f49429968235b836217aaca5610
nitinchoudhary2 <namejaatboys@gmail.com>
2025-11-01 12:02:09 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'truvani-v1';
-const ASSETS = ['/', '/index.html', '/quantum.html', '/admin.html', '/manifest.json'];
+const CACHE_NAME = 'truvani-v1';
+const urlsToCache = ['/', '/index.html', '/admin.html'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
