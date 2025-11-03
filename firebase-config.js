30928bc44ebc9f40eb7a76eb99798e0788c9479c
nitinchoudhary2 <namejaatboys@gmail.com>
2025-11-03 16:31:03 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,19 @@
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, serverTimestamp, doc, updateDoc, deleteDoc, addDoc, collection, query, orderBy, where, getDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAeGWwKLkHB43i1FbedgkANPKcTCBh0Z9A",
-  authDomain: "truvani-news-5ac15.firebaseapp.com",
-  projectId: "truvani-news-5ac15",
-  storageBucket: "truvani-news-5ac15.appspot.com",
-  messagingSenderId: "742142167141",
-  appId: "1:742142167141:web:c2c116f5e50a574de85e8c"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
-export { serverTimestamp, doc, updateDoc, deleteDoc, addDoc, collection, query, orderBy, where, getDoc, getDocs, ref, uploadBytes, getDownloadURL };
